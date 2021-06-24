377e42209a7c60b14f8178b8179f8f6e5c312565
Chitvan Singh <ChitvanSingh2109@gmail.com>
2021-06-24 12:03:02 -0400

    Fixed Stuff in Index 3

--- CodeTheWay/CodeTheWay.Web.Ui/Models/ViewModels/BarrelViewModel.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Models/ViewModels/BarrelViewModel.cs
@@ -9,6 +9,8 @@ namespace CodeTheWay.Web.Ui.Models.ViewModels
     public class BarrelViewModel
     {
         public Guid Id { get; set; }
+        public double Radius { get; set; }
+        public double Height { get; set; }
         public DateTime DateCreated { get; set; }
         public Contents Contents { get; set; }
         public string CurrentLocation { get; set; }
