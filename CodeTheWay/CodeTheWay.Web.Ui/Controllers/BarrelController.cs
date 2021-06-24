27f40e37092a005c69323a9e9e2b28f527abc3ff
Elijah Bansley <elijahbansley@gmail.com>
2021-06-24 10:30:47 -0500

    Add Details, Index, and Enums for ViewModel

--- CodeTheWay/CodeTheWay.Web.Ui/Controllers/BarrelController.cs
+++ CodeTheWay/CodeTheWay.Web.Ui/Controllers/BarrelController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeTheWay.Web.Ui.Models.Enums;
+using CodeTheWay.Web.Ui.Models.ViewModels;
 
 namespace CodeTheWay.Web.Ui.Controllers
 {
@@ -16,9 +18,32 @@ namespace CodeTheWay.Web.Ui.Controllers
         {
             this.BarrelService = BarrelService;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var barrels = await BarrelService.GetBarrels();
+            var viewModels = new List<BarrelViewModel>();
+            foreach (var barrel in barrels)
+            {
+                var volume = Math.PI * (barrel.Radius * barrel.Radius) * barrel.Height;
+
+                
+                viewModels.Add(new BarrelViewModel()
+                {
+                    Id = barrel.Id,
+                    Contents = (Contents) barrel.Contents,
+                    CurrentLocation = barrel.CurrentLocation,
+                    DateCreated = barrel.DateCreated,
+                    Size = Size.Small,
+                });
+            }
+
+            return View(model: viewModels);
+        }
+
+        public async Task<IActionResult> Details(Guid id)
+        {
+            return View(await BarrelService.GetBarrel(id));
         }
     }
 }
---
+++ CodeTheWay/CodeTheWay.Web.Ui/Models/Enums/Contents.cs
@@ -0,0 +1,11 @@
+namespace CodeTheWay.Web.Ui.Models.Enums
+{
+    public enum Contents
+    {
+        Documents,
+        Food,
+        Supplies,
+        Machinery,
+        Misc
+    }
+}
